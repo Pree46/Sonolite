e636538423667aa7a8d9ecc62d63c5bfde9644f4
Ritul Bharati <ritul.pravash@gmail.com>
2025-03-10 17:39:30 +0530

    Splash Screen added

---
+++ sonolite_app/app/src/main/java/com/example/sonolite_app/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.sonolite_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Move to the main activity after 2 seconds
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Finish SplashActivity so user can't go back to it
+        }, 2000);
+    }
+}
